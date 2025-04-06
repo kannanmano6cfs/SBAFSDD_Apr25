f84a86622527e59aca4c3816d4f98e792531a45f
bhaveshbfss <bhaveshb@fsstech.com>
2025-04-06 20:44:57 +0530

    Delete SB_Demos/Bhavesh/bestbank/bestbank directory

--- SB_Demos/Bhavesh/bestbank/bestbank/src/main/java/com/bank/bestbank/ExceptionHandling/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bank.bestbank.ExceptionHandling;
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(int id) {
-        super("Product with id " + id + " not found");
-    }
-}
--- SB_Demos/Bhavesh/bestbank/bestbank/src/main/java/com/bank/bestbank/Repository/bankRepo.java
+++
@@ -1,8 +0,0 @@
-package com.bank.bestbank.Repository;
-
-import com.bank.bestbank.Model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface bankRepo  extends CrudRepository<Product,Integer> {
-}
-
