220de83907ed7876b1251a393db0d58b690f4291
bhaveshbfss <bhaveshb@fsstech.com>
2025-04-08 00:54:32 +0530

    Add files via upload

---
+++ SB_Demos/Bhavesh/MyAPP3/src/services/todo.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+    constructor() { }
+
+    //shared data
+    private items: string[] = []
+
+    addItem(item: string){
+      this.items.push(item)
+    }
+
+    getItems(): string[]{
+      return this.items;
+    }
+}
